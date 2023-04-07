993eb5ff04b563308d84711a2adf7acbecf78d5a
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-04-07 15:39:02 +0530

    Commited some styles changes

--- admission form/admission.js
+++ admission form/admission.js
@@ -29,16 +29,16 @@ let container3Styles = {
 
 Object.assign(document.getElementById('container3').style, container3Styles);
 
-// row design--->
+// admission design--->
 
-let lableStyles = {
+let admissionStyles = {
     color: '#000',
     textTransform:'uppercase',
     fontSize: '32px',
     letterSpacing: '13px',
 
 }
-Object.assign(document.getElementById('admissson').style, lableStyles);
+Object.assign(document.getElementById('admissson').style, admissionStyles);
 // row design--->
 
 let rowStyles = {
@@ -52,7 +52,8 @@ document.querySelectorAll('.row').forEach(element => { return Object.assign(elem
 // form design--->
 let formStyles = {
     padding:'50px 200px',
-    backgroundColor: '#F1F1F1',
+    backgroundImage: 'url("bg.jpg")',
+    backgroundRepeat : 'none',
     boxSizing: 'border-box',
 }
 Object.assign(document.querySelector('form').style, formStyles);
@@ -67,4 +68,5 @@ let inputStyles = {
     boxSizing: 'border-box',
 }
 document.querySelectorAll('input').forEach(element => { return Object.assign(element.style, inputStyles); });
+
 Object.assign(document.querySelector('select').style, inputStyles);
